d579677769c74dcd2a8ab7dce4723029dddcfd71
U_35007842 <U_35007842@DESKTOP-QVMOKB8.mshome.net>
2019-02-13 11:34:54 +0000

    "version original para dani"

---
+++ src/mates/potencia.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mates;
+
+/**
+ *
+ * @author U_35007842
+ */
+public class potencia {
+   
+    public int potencia(int n1 ,int n2){
+     int resultado = (int) Math.pow(n1, n2);
+       return resultado;
+    }
+}
